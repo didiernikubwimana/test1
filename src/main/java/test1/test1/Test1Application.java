4cda8f418a3b35e1bedc9054ce45b6ca7509ee4d
didiernikubwimana <57959937+didiernikubwimana@users.noreply.github.com>
2020-12-06 19:02:15 -0600

    mesg 2
    dhsjdhgsjdfgsjf
    
    done

--- src/main/java/test1/test1/Test1Application.java
+++ src/main/java/test1/test1/Test1Application.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test1Application {
 
 	public static void main(String[] args) {
+		System.out.print ("testing  sdksjkdhsfwsdf");
 		SpringApplication.run(Test1Application.class, args);
+		System.out.print ("done");
 	}
 
 }
